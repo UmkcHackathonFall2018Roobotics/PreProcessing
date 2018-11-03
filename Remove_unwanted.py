235ee34853e6fd0bbedd037f62bd80a7958019ca
Angad Pal Singh <43454863+angad-singh15@users.noreply.github.com>
2018-11-03 14:28:27 -0500

    Added Remove_unwanted.py
    Removes stopwords and cleans out the code

---
+++ Remove_unwanted.py
@@ -0,0 +1,32 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+
+def clean_text(text):
+    
+    # Convert words to lower case
+    text = text.lower()
+    
+    # Format words and remove unwanted characters
+    text = re.sub(r'&amp;', '', text) 
+    text = re.sub(r'0,0', '00', text) 
+    text = re.sub(r'[_"\-;%()|.,+&=*%.,!?:#@\[\]]', ' ', text)
+    text = re.sub(r'\'', ' ', text)
+    text = re.sub(r'\$', ' $ ', text)
+    #replace abbreviations 
+    text = re.sub(r'u s ', ' united states ', text)
+    text = re.sub(r'u n ', ' united nations ', text)
+    text = re.sub(r'u k ', ' united kingdom ', text)
+    text = re.sub(r'j k ', ' jk ', text)
+    text = re.sub(r' s ', ' ', text)
+    text = re.sub(r' yr ', ' year ', text)
+    text = re.sub(r' l g b t ', ' lgbt ', text)
+    text = re.sub(r'0km ', '0 km ', text)
+
+    #remove stop words
+    text = text.split()
+    stops = set(stopwords.words("english")) #using the english library of stopwords
+    text = [w for w in text if not w in stops]
+    text = " ".join(text)
+
+    return text
