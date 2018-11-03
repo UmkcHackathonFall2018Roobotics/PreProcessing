f8be450fd3c3f6b47c9fb9807dea84cb2571104f
Nate314 <NathanGawith@gmail.com>
2018-11-03 18:34:12 -0500

    added column to the end of our csv file
    added a 0 or 1 to the end for the direction of price movement.

---
+++ add_binary_prices_to_sentiment_values.py
@@ -0,0 +1,48 @@
+import csv;
+
+if __name__ == "__main__":
+	
+	up_or_down = {};
+	with open('Combined_News_DJIA.csv', newline='\n') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter=',', quotechar='"');
+		for row in csvreader:
+			print("reading" + row[0]);
+			if row[0].startswith("20"):
+				date = row[0];
+				direction = row[1];
+				up_or_down[date] = direction;
+			
+	print(up_or_down);
+	
+	content = "";
+	with open('sentiment_values_binary.csv', newline='\n') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter=',', quotechar='"');
+		for row in csvreader:
+			try:
+				direction = "";
+				print("writing" + row[0]);
+				count = 0;
+				if row[0].startswith("20"):
+					date = row[0];
+					direction = up_or_down[date];
+					for data in row:
+						content += data + ",";
+						
+					content += direction;
+					
+				else:
+					count = 0;
+					for data in row:
+						content += data;
+						if not count == len(row) - 1:
+							content += ",";
+						
+					
+				content += "\n";
+			except:
+				print("exception");
+			
+		
+	with open("sentiment_priceaction_combined_binary.csv", "w+") as writer:
+		writer.write(content);
+	
